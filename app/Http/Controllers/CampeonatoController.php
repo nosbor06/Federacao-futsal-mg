4c9f52f4fa1c00005f4426fe1ecb383562257865
FabioBarrosMiranda <fabinhomiranda324@gmail.com>
2026-03-05 21:31:02 -0300

    feat - CRUD campeonato finalizado

--- app/Http/Controllers/CampeonatoController.php
+++ app/Http/Controllers/CampeonatoController.php
@@ -2,52 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use App\Models\Campeonato;
 
 class CampeonatoController extends Controller
 {
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-| Index |=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
     public function index()
     {
         $campeonatos = Campeonato::all();
-
         return view('campeonatos.index', compact('campeonatos'));
     }
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-| Create |=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
     public function create()
     {
         return view('campeonatos.create');
     }
 
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-| Store |=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
     public function store(Request $request)
     {
         $validated = $request->validate([
-        'nome' => ['required', 'string', 'max:100'],
-        'categoria' => ['required', 'string', 'max:50'],
-        'data_inicio' => ['required', 'date'],
-        'data_fim' => ['required', 'date', 'after_or_equal:data_inicio'],
-        'status' => ['required', 'in:inscricoes_abertas,em_andamento,encerrado,cancelado'],
-    ]);
+            'nome' => ['required', 'string', 'max:100'],
+            'categoria' => ['required', 'string', 'max:50'],
+            'data_inicio' => ['required', 'date'],
+            'data_fim' => ['required', 'date', 'after_or_equal:data_inicio'],
+            'status' => ['required', 'in:inscricoes_abertas,em_andamento,encerrado,cancelado'],
+        ]);
 
-    Campeonato::create($validated);
-    return redirect()->route('campeonatos.index')->with('success','O campeonato foi cadastrado com sucesso!');
+        Campeonato::create($validated);
+
+        return redirect()->route('campeonatos.index')
+            ->with('success','O campeonato foi cadastrado com sucesso!');
     }
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-| Edit |=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
     public function edit(Campeonato $campeonato)
     {
-        return view('campeonatos.edit', compact('campeonatos'));
+        return view('campeonatos.edit', compact('campeonato'));
     }
-
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-| Update |=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
     public function update(Request $request, Campeonato $campeonato)
     {
@@ -60,14 +51,16 @@ class CampeonatoController extends Controller
         ]);
 
         $campeonato->update($validated);
-        return redirect()->route('campeonatos.edit')->with('success','O campeonato foi atualizado com sucesso!');
-    }
 
-    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-| Destroy |=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+        return redirect()->route('campeonatos.index')
+            ->with('success','O campeonato foi atualizado com sucesso!');
+    }
 
     public function destroy(Campeonato $campeonato)
     {
-        $campeonato-> delete();
-        return redirect()->route('campeonatos.edit')->with('success','O campeonato foi excluido com sucesso!');
+        $campeonato->delete();
+
+        return redirect()->route('campeonatos.index')
+            ->with('success','O campeonato foi excluído com sucesso!');
     }
 }
